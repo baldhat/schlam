7266c54dec4af86dea54c36152085e2f08ea3c7f
baldhat <michel.jonathan@proton.me>
2025-12-19 16:30:46 +0100

    Add c++

---
+++ schlam/cpp/src/main.cpp
@@ -0,0 +1,9 @@
+#include <print>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> v = {1, 2, 3};
+    std::println("Hello C++23! Vector size: {}", v.size());
+    return 0;
+}
